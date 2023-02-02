d8146e6b88ef87962f897a13b5a74fd5b27e9f86
HeleneGSK <helene.x.fourmanoir@gsk.com>
2023-02-02 12:33:59 +0000

    change user

--- link_git.R
+++ link_git.R
@@ -1,11 +1,11 @@
 install.packages("usethis")
-usethis::use_git_config(user.name= "hf69578",
+usethis::use_git_config(user.name= "HeleneGSK",
                         user.email="helene.x.fourmanoir@gsk.com")
 usethis::use_git()
 usethis::create_github_token()
 
 
 
-gitcreds::gitcreds_set()
+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
 
 usethis::use_github()
