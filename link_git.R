874f65e2cf77b4df8eb432bc951c922b10e76d5a
hf69578 <helene.x.fourmanoir@gsk.com>
2023-02-02 12:19:55 +0000

    commands used for the link to git

---
+++ link_git.R
@@ -0,0 +1,11 @@
+install.packages("usethis")
+usethis::use_git_config(user.name= "hf69578",
+                        user.email="helene.x.fourmanoir@gsk.com")
+usethis::use_git()
+usethis::create_github_token()
+
+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
+
+gitcreds::gitcreds_set()
+
+usethis::use_github()
