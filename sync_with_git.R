8324299b66b5d0e0154defe735cc550f92fef341
fontesse <alain.x.fontesse@gsk.com>
2023-02-20 09:02:43 +0000

    commit

--- sync_with_git.R
+++ sync_with_git.R
@@ -1,7 +1,9 @@
-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
+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
 gitcreds::gitcreds_set()
 usethis::use_git_config(user.name= "fontesse",user.email="alain.x.fontesse@gsk.com")
 
 
 
 
+usethis::use_git_config(user.name= "HeleneGSK", user.email="helene.x.fourmanoir@gsk.com")
+usethis::create_github_token()
