0fa3ac0c333b098b217f89f1b08121e57a111dc2
HeleneGSK <helene.x.fourmanoir@gsk.com>
2023-02-20 15:07:05 +0000

    add a new token

--- link_git.R
+++ link_git.R
@@ -5,6 +5,9 @@ usethis::use_git()
 usethis::create_github_token()
 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
 
+#new token:
+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
+
 gitcreds::gitcreds_set()
 
 usethis::use_github()
