d951bc69893ef9cf034d53fef9c9b0ac2eeab4ba
HeleneGSK <helene.x.fourmanoir@gsk.com>
2023-02-27 13:39:21 +0000

    after 30 submission

--- programs/adlbc.R
+++ programs/adlbc.R
@@ -15,50 +15,50 @@ lb1 <- lb %>%
 
 param_lookup <- tibble::tribble(
   ~LBTESTCD, ~PARAMCD, ~PARAM, ~PARAMN,
-  "ALB", "ALB", "Albumin (g/L)", 1,
-  "ALP", "ALP", "Alkaline Phosphatase (U/L)", 2,
-  "ALT", "ALT", "Alanine Aminotransferase (U/L)", 3,
+  "ALB", "ALB", "Albumin (g/L)", 33,
+  "ALP", "ALP", "Alkaline Phosphatase (U/L)", 22,
+  "ALT", "ALT", "Alanine Aminotransferase (U/L)", 24,
   "ANISO", "ANISO", "Anisocytes", 4,
-  "AST", "AST", "Aspartate Aminotransferase (U/L)", 5,
+  "AST", "AST", "Aspartate Aminotransferase (U/L)",25,
   "BASO", "BASO", "Basophils (10^9/L)", 6,
   "BASOLE", "BASOLE", "Basophils/Leukocytes (FRACTION)", 7,
-  "BILI", "BILI", "Bilirubin (umol/L)", 8,
-  "BUN", "BUN", "Blood Urea Nitrogen (mmol/L)", 9,
-  "CA", "CA", "Calcium (mmol/L)", 10,
-  "CHOL", "CHOL", "Cholesterol (mmol/L)", 11,
-  "CK", "CK", "Creatinine Kinase (U/L)", 12,
-  "CL", "CL", "Chloride (mmol/L)", 13,
+  "BILI", "BILI", "Bilirubin (umol/L)", 21,
+  "BUN", "BUN", "Blood Urea Nitrogen (mmol/L)", 26,
+  "CA", "CA", "Calcium (mmol/L)", 30,
+  "CHOL", "CHOL", "Cholesterol (mmol/L)", 34,
+  "CK", "CK", "Creatine Kinase (U/L)", 35,
+  "CL", "CL", "Chloride (mmol/L)", 20,
   "COLOR", "COLOR", "Color", 14,
-  "CREAT", "CREAT", "Creatinine (umol/L)", 15,
+  "CREAT", "CREAT", "Creatinine (umol/L)", 27,
   "EOS", "EOS", "Eosinophils (10^9/L)", 16,
   "EOSLE", "EOSLE", "Eosinophils/Leukocytes (FRACTION)", 17,
-  "GGT", "GGT", "Gamma Glutamyl Transferase (U/L)", 18,
-  "GLUC", "GLUC", "Glucose (mmol/L)", 19,
-  "HBA1C", "HBA1C", "Hemoglobin A1C (1)", 20,
-  "HCT", "HCT", "Hematocrit (1)", 21,
-  "HGB", "HGB", "Hemoglobin (mmol/L)", 22,
-  "K", "K", "Potassium (mmol/L)", 23,
-  "KETONES", "KETONES", "Ketones", 24,
-  "LYM", "LYM", "Lymphocytes (10^9/L)", 25,
-  "LYMLE", "LYMLE", "Lymphocytes/Leukocytes (FRACTION)", 26,
-  "MACROCY", "MACROCY", "Macrocytes", 27,
-  "MCH", "MCH", "Ery. Mean Corpuscular Hemoglobin (fmol(Fe))", 28,
-  "MCHC", "MCHC", "Ery. Mean Corpuscular HGB Concentration (mmol/L)", 29,
-  "MCV", "MCV", "Ery. Mean Corpuscular Volume (f/L)", 30,
-  "MICROCY", "MICROCY", "Microcytes", 31,
-  "MONO", "MONO", "Monocytes (10^9/L)", 32,
-  "MONOLE", "MONOLE", "Monocytes/Leukocytes (FRACTION)", 33,
-  "PH", "PH", "pH", 34,
-  "PHOS", "PHOS", "Phosphate (mmol/L)", 35,
+  "GGT", "GGT", "Gamma Glutamyl Transferase (U/L)", 23,
+  "GLUC", "GLUC", "Glucose (mmol/L)", 31,
+  "HBA1C", "HBA1C", "Hemoglobin A1C (1)", 13,
+  "HCT", "HCT", "Hematocrit (1)", 8,
+  "HGB", "HGB", "Hemoglobin (mmol/L)", 2,
+  "K", "K", "Potassium (mmol/L)", 19,
+  "KETONES", "KETONES", "Ketones", 3,
+  "LYM", "LYM", "Lymphocytes (10^9/L)", 5,
+  "LYMLE", "LYMLE", "Lymphocytes/Leukocytes (FRACTION)", 9,
+  "MACROCY", "MACROCY", "Macrocytes", 15,
+  "MCH", "MCH", "Ery. Mean Corpuscular Hemoglobin (fmol(Fe))", 44,
+  "MCHC", "MCHC", "Ery. Mean Corpuscular HGB Concentration (mmol/L)", 12,
+  "MCV", "MCV", "Ery. Mean Corpuscular Volume (f/L)", 10,
+  "MICROCY", "MICROCY", "Microcytes", 41,
+  "MONO", "MONO", "Monocytes (10^9/L)", 39,
+  "MONOLE", "MONOLE", "Monocytes/Leukocytes (FRACTION)", 1,
+  "PH", "PH", "pH", 11,
+  "PHOS", "PHOS", "Phosphate (mmol/L)", 29,
   "PLAT", "PLAT", "Platelet (10^9/L)", 36,
   "POIKILO", "POIKILO", "Poikilocytes", 37,
   "POLYCHR", "POLYCHR", "Polychromasia", 38,
-  "PROT", "PROT", "Protein (g/L)", 39,
+  "PROT", "PROT", "Protein (g/L)", 32,
   "RBC", "RBC", "Erythrocytes (TI/L)", 40,
-  "SODIUM", "SODIUM", "Sodium (mmol/L)", 41,
+  "SODIUM", "SODIUM", "Sodium (mmol/L)", 18,
   "SPGRAV", "SPGRAV", "Specific Gravity", 42,
   "TSH", "TSH", "Thyrotropin (mU/L)", 43,
-  "URATE", "URATE", "Urate (umol/L)", 44,
+  "URATE", "URATE", "Urate (umol/L)", 28,
   "UROBIL", "UROBIL", "Urobilinogen", 45,
   "VITB12", "VITB12", "Vitamin B12 (pmol/L)", 46,
   "WBC", "WBC", "Leukocytes (10^9/L)", 47
@@ -111,12 +111,24 @@ mutate(
   TRTP = TRT01P,
   TRTPN = TRT01PN,
   TRTA = TRT01A,
-  TRTAN = TRT01AN
+  TRTAN = TRT01AN,
+  ADT = as.numeric(ADT-as.Date('1960-01-01') ),
+  TRTEDT = as.numeric(TRTEDT-as.Date('1960-01-01') ),
+  TRTSDT= as.numeric(TRTSDT-as.Date('1960-01-01') ),
+  AVISIT= VISIT,
+  AVISITN= VISITNUM
 )
 
+
 adlb <- adlb %>%
-  mutate(ALBTRVAL= max(((1.5*A1HI)-LBSTRESN) , (LBSTRESN-(.5*A1LO)))
-)
+  mutate(ALBTRVAL1= ((1.5*A1HI)-LBSTRESN)
+  )
+adlb <- adlb %>%
+  mutate(ALBTRVAL2=  (LBSTRESN-(.5*A1LO))
+  )
+adlb <- adlb %>%
+  mutate(ALBTRVAL = if_else(ALBTRVAL1 >ALBTRVAL2 , ALBTRVAL1, ALBTRVAL2))
+
 
 adlb <- adlb %>%
   mutate(
@@ -129,16 +141,36 @@ adlb <- adlb %>%
   )
 
 adlb <- adlb %>%
-  # Derive Timing
   mutate(
     AVISIT = case_when(
-      str_detect(VISIT, "SCREENING 1") ~ "Baseline",
-      !is.na(VISIT) ~ str_to_title(VISIT),
-      TRUE ~ NA_character_
-    ),
+      VISITNUM == 1 ~ 'Baseline',
+      VISITNUM == 9 ~ 'Week 12',
+      VISITNUM == 10 ~ 'Week 16',
+      VISITNUM == 4 ~ 'Week 2',
+      VISITNUM == 5 ~ 'Week 4',
+      VISITNUM == 7 ~ 'Week 6',
+      VISITNUM == 8 ~ 'Week 8',
+      VISITNUM == 11 ~ 'Week 20',
+      VISITNUM == 12 ~ 'Week 24',
+      VISITNUM == 13 ~ 'Week 26',
+
+    )
+  )
+
+adlb <- adlb %>%
+  mutate(
     AVISITN = case_when(
-      AVISIT == "Baseline" ~ 0,
-      !is.na(VISITNUM) ~ VISITNUM
+      VISITNUM == 1 ~ 0,
+      VISITNUM == 9 ~ 12,
+      VISITNUM == 10 ~ 16,
+      VISITNUM == 4 ~ 2,
+      VISITNUM == 5 ~ 4,
+      VISITNUM == 7 ~ 6,
+      VISITNUM == 8 ~ 8,
+      VISITNUM == 11 ~ 20,
+      VISITNUM == 12 ~ 24,
+      VISITNUM == 13 ~ 26,
+
     )
   )
 adlb <- adlb %>%
@@ -150,6 +182,13 @@ adlb <- adlb %>%
 adlb <- adlb %>%
   derive_var_anrind()
 
+adlb <- adlb %>%
+  mutate(
+    ANRIND = case_when(
+      ANRIND == 'NORMAL' ~ 'N'
+
+    )
+  )
 ## Derive baseline flags ----
 # adlb <- adlb %>%
 #   # Calculate BASETYPE
@@ -171,8 +210,8 @@ adlb <- adlb %>%
   mutate(ABLFL = if_else(VISITNUM==1, 'Y', ''))
 
 adlb <- adlb %>%
-  mutate(R2A1HI = AVAL/A1LO,
-         R2A1LO = AVAL/A1HI)
+  mutate(R2A1HI = AVAL/A1HI,
+         R2A1LO = AVAL/A1LO)
 
 ## Derive baseline information ----
 adlb <- adlb %>%
@@ -200,8 +239,11 @@ adlb <- adlb %>%
   derive_var_pchg()
 
 adlb <- adlb %>%
-  mutate(BR2A1HI = BASE/A1LO,
-         BR2A1LO = BASE/A1HI)
+  mutate(BR2A1HI = BASE/A1HI,
+         BR2A1LO = BASE/A1LO)
+
+adlb <- adlb %>%
+  mutate(CHG = if_else(VISITNUM==1, NA, CHG))
 
 adlb <- adlb %>%
 
@@ -248,9 +290,9 @@ adlb <- adlb %>%
   # get MAXIMUM value
   derive_extreme_records(
     by_vars = vars(STUDYID, USUBJID, PARAMCD),
-    order = vars( ADT, AVISITN),
+    order = vars( ADT, VISITNUM),
     mode = "last",
-    filter = (!is.na(AVAL) & VISITNUM >= 2 & AVISITN <= 12),
+    filter = (!is.na(AVAL) & VISITNUM >= 2 & VISITNUM <= 12),
     set_values_to = vars(
       AVISITN = 99,
       AVISIT = "End of Treatment",
@@ -292,6 +334,7 @@ adlbc <- select(adlb,
                 A1LO,
                 A1HI,
                 R2A1LO,
+                R2A1HI,
                 BR2A1LO,
                 BR2A1HI,
                 ANL01FL,
@@ -303,4 +346,14 @@ adlbc <- select(adlb,
                 LBSEQ,
                 LBNRIND,
                 LBSTRESN)
+
+adlbc_spec <-readxl::read_excel("metadata/specs.xlsx", sheet = "Variables")%>%
+  filter(Dataset == "ADLBC")
+adlbc_spec <- subset(adlbc_spec, select = c(Dataset, Variable, Label))
+adlbc_spec <- rename(adlbc_spec, label=Label)
+adlbc_spec <- rename(adlbc_spec, dataset=Dataset)
+adlbc_spec <- rename(adlbc_spec, variable=Variable)
+adlbc <- adlbc %>%
+  xportr_label(adlbc_spec, domain = "ADLBC") # Assigns variable label from metacore specifications
+
  xportr_write(adlbc, "adam/adlbc.xpt")
